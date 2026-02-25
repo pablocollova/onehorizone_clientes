b2d048c9e07fb1200c3dcb7f5d7777291754729a
Pablo Ariel Collova <pablocollova@gmail.com>
2026-02-25 16:14:58 +0100

    update

---
+++ backend/prisma.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+import path from "path";
+
+// fuerza carga del .env que está en backend/.env
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: { path: "prisma/migrations" },
+  datasource: { url: process.env.DATABASE_URL },
+});
