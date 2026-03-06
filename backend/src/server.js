73e28d8878017e7ba10d200e9c2c334e1355ea7a
Pablo Ariel Collova <pablocollova@gmail.com>
2026-03-06 12:00:53 +0100

    feat: Implement initial server setup for the backend application.

--- backend/src/server.js
+++ backend/src/server.js
@@ -65,6 +65,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`✅ API listening on http://localhost:${port}`);
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`✅ API listening on 0.0.0.0:${port}`);
 });
